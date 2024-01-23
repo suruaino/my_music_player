e42414ddd0fbbcd5e5203ef1f794569a92c6c9ca
SURU EMMANUEL <suruaino@gmail.com>
2024-01-23 16:56:45 +0100

    Added music list button

--- src/components/Main/Song_modes/Modes.jsx
+++ src/components/Main/Song_modes/Modes.jsx
@@ -6,6 +6,9 @@ const Modes = () => {
             <button id='mode'><i class="fa fa-repeat" aria-hidden="true"></i></button>
             <button id='favorites'><i class="fa fa-heart" aria-hidden="true"></i></button>
             <button id='shuffle'><i class="fa-solid fa-shuffle"></i></button>
+            <button id='song_list'>
+                <div className='bar'></div>
+            </button>
         </div>
     )
 }
